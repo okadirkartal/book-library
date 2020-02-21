916bdaca554eee4edb589e162ff114c73169858c
okadirkartal <okadirkartal@gmail.com>
2020-02-22 00:44:01 +0200

    models,data and services files are added

---
+++ Data/Services/IBookService.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using BookLibrary.Data;
+
+namespace BookLibrary.Data
+{
+    public interface IBookService
+    {
+        List<Book> GetAllBooks();
+        Book GetBookById(int id);
+        void UpdateBook(int id, Book newBook);
+        void DeleteBook(int id);
+        void AddBook(Book newBook);
+    }
+}
